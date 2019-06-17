9bf51588ec8c724032698b529612a01ca5be6c0c
Mercy Nekesa <kesamercy@gmail.com>
2019-06-16 22:00:02 -0400

    splash page

---
+++ createaccount.php
@@ -0,0 +1,27 @@
+<?php 
+
+include 'DB.php';
+
+if(isset($_POST['createaccount'])){
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+
+    DB::query('INSERT INTO users VALUES (\'\', :username, :password, :email)', array(':username'=>$username, ':password'=> $password, ':email'=>$email));
+
+    echo "Success!";
+}
+
+ ?>
+
+
+
+ <h1> Register </h1>
+     
+ <form action="createaccount.php" method="post">
+    <input type="text" name="username" value="" placeholder="Username..."> <p />
+    <input type="password" name="password" value="" placeholder="Password...">
+    <input type="email" name="email" value="" placeholder="someone@gmail.com">
+    <input type="submit" name="createaccount" value="Create Account">
+ </form>
