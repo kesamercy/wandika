96e65117f36cf8002d95fd3995a19fa8d9b2b747
Mercy Nekesa <kesamercy@gmail.com>
2019-07-04 16:15:00 -0400

    modified version of login files

--- loginsystem/includes/login.inc.php
+++ loginsystem/includes/login.inc.php
@@ -5,27 +5,27 @@ session_start();
 if(isset($_POST['submit'])){
 
 	include 'dbh.inc.php';
-	$uid= mysqli_real_escape_string($conn, $_POST['uid']); //mysqli_real_escape_string() makes sure that people do not type in any malicious code inside.
-	$pwd= mysqli_real_escape_string($conn, $_POST['pwd']);
+	$uid= mysqli_real_escape_string($conn, $_POST['username']); //mysqli_real_escape_string() makes sure that people do not type in any malicious code inside.
+	$pwd= mysqli_real_escape_string($conn, $_POST['user_pass']);
 
 	//Error handlers
 	//Check if inputs are empty
 	if(empty($uid) || empty($pwd)){
-         header("Location: ../index.php?login=empty");
+         header("Location: ../spalashpage.html?login=empty");
 	     exit();
 	}else{
 		$sql= "SELECT * FROM user WHERE username='$uid' OR email='$uid'";
 		$result=mysqli_query($conn, $sql);
 		$resultCheck= mysqli_num_rows($result);
 		if($resultCheck<1){
-			header("Location: ../index.php?login=error");
+			header("Location: ../spalashpage.html?login=error");
 	        exit();
 		}else{
 			if($row = mysqli_fetch_assoc($result))
 			//De-hashing the password
 			$hashedPwdCheck= password_verify($pwd, $row['password']); //matching the database password with the password entered by the user using inbuilt password_verify().
 		    if($hashedPwdCheck == false){
-		    	header("Location: ../index.php?login=error");
+		    	header("Location: ../spalashpage.html?login=error");
 	            exit();
 		    } elseif($hashedPwdCheck == true){
 		    	//Log in the user here
@@ -34,12 +34,12 @@ if(isset($_POST['submit'])){
 		    	$_SESSION['u_email'] = $row['email'];
 		    	$_SESSION['u_pwd'] = $row['password'];
 		    	$_SESSION['u_cntry'] = $row['country'];
-		    	header("Location: ../index.php?login=success");
+		    	header("Location: ../spalashpage.html?login=success");
 	            exit();
 		    }
 		}
 	}
 }else{
-	header("Location: ../index.php?login=error");
+	header("Location: ../spalashpage.html?login=error");
 	exit();
 }
