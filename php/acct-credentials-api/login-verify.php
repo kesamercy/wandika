9ba2f3fd315aeb131f58d747befd09e993f98613
Mercy Nekesa <kesamercy@gmail.com>
2019-07-17 21:49:37 -0400

    modified changes

--- php/acct-credentials-api/login-verify.php
+++ php/acct-credentials-api/login-verify.php
@@ -11,21 +11,21 @@ if(isset($_POST['submit'])){
 	//Error handlers
 	//Check if inputs are empty
 	if(empty($uid) || empty($pwd)){
-         header("Location: ../spalashpage.html?login=empty");
+         header("Location: ../html/welcome-page.html?login=error");
 	     exit();
 	}else{
 		$sql= "SELECT * FROM user WHERE username='$uid' OR email='$uid'";
 		$result=mysqli_query($conn, $sql);
 		$resultCheck= mysqli_num_rows($result);
 		if($resultCheck<1){
-			header("Location: ../spalashpage.html?login=error");
+			header("Location: ../html/welcome-page.html?login=error");
 	        exit();
 		}else{
 			if($row = mysqli_fetch_assoc($result))
 			//De-hashing the password
 			$hashedPwdCheck= password_verify($pwd, $row['password']); //matching the database password with the password entered by the user using inbuilt password_verify().
 		    if($hashedPwdCheck == false){
-		    	header("Location: ../spalashpage.html?login=error");
+		    	header("Location: ../html/welcome-page.html?login=error");
 	            exit();
 		    } elseif($hashedPwdCheck == true){
 		    	//Log in the user here
@@ -34,12 +34,12 @@ if(isset($_POST['submit'])){
 		    	$_SESSION['u_email'] = $row['email'];
 		    	$_SESSION['u_pwd'] = $row['password'];
 		    	$_SESSION['u_cntry'] = $row['country'];
-		    	header("Location: ../spalashpage.html?login=success");
+		    	header("Location: ../html/newsfeed.html?login=success");
 	            exit();
 		    }
 		}
 	}
 }else{
-	header("Location: ../spalashpage.html?login=error");
+	header("Location: ../html/welcome-page.html?login=error");
 	exit();
 }
