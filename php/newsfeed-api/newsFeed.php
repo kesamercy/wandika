4810febd424bbbd7a388b0c05cf89372ec82ddbe
Jared Lockhart <lockhartjared@gmail.com>
2019-07-25 22:03:42 -0400

    Code with my test functions.

--- php/newsfeed-api/newsFeed.php
+++ php/newsfeed-api/newsFeed.php
@@ -5,10 +5,39 @@
     // the response will be a JSON object
     header('Content-Type: application/json');
     header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");$json = array();
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     // pull the input, which should be in the form of a JSON object
     //$json_params = file_get_contents('php://input');
    
+    // if(!isset($_POST['functionname'])){
+    //      $aResult['error'] = 'No function name!'; 
+    //     }
+
+    // if(!isset($_POST['arguments'])){ 
+    //     $aResult['error'] = 'No function arguments!';
+    // }
+    //$decoded_params = json_decode($json_params, TRUE);
+    //$action = $decoded_params['action'];
+   // $json['action'] = $action;
+    // if( !isset($aResult['error']) ) {
+        //$function_name = 'blog-post';
+
+        if(isset($_POST['action']) && !empty($_POST['action'])) {
+
+            $action = $_POST['action'];
+                switch($action) {
+                    case 'blog-post': echo "set blog post test"; set_blog_post_test(); break;
+                    case 'tip-post': echo "set tag post"; set_tip_test(); break;
+                    default: echo "No such function";
+                }
+        
+        }
+            
+     
+
+    //}
+
+    //echo json_encode($aResult);
     /**
      * Function that accepts a written post from an html form and associates it with the specified user_id.
      * The function will take the necessary parts from the post fields and input them into the sql database. Uses PDO and
@@ -21,18 +50,18 @@
         $conn = connect();
         
         //prepare statement
-        $sql = "INSERT INTO posts_table (user_id, title, content, genre, date_posted, time_read, post_image, post_type, allow_comments)
-        VALUES(:user_id, :title, :post, :genre, :`data`, :time_read, :post_image, :post_type, :allow_comments)";
+        $sql = "INSERT INTO `posts_table` (user_id, title, content, genre, date_posted, time_read, post_image, post_type, allow_comments)
+        VALUES(:user_id, :title, :post, :genre, :'date', :time_read, :post_image, :post_type, :allow_comments)";
         $conn->prepare($sql);
-        $conn->bindParam(`:user_id`, $user_id); //From session ID
-        $conn->bindParam(`:title`, $title);
-        $conn->bindParam(`:content`, $written_post); //from text box
-        $conn->bindParam(`:genre`, $genre);
-        $conn->bindParam(`:date_posted`, $date); //dont worry about this. From php date function
-        $conn->bindParam(`:time_read`, $time_to_read);
-        $conn->bindParam(`:post_image`, $post_image);
-        $conn->bindParam(`:post_type`, $type_of_post);
-        $conn->bindParam(`:allow_comments`, $allow_comments);
+        $conn->bindParam(':user_id', $user_id); //From session ID
+        $conn->bindParam(':title', $title);
+        $conn->bindParam(':content', $written_post); //from text box
+        $conn->bindParam(':genre', $genre);
+        $conn->bindParam(':date_posted', $date); //dont worry about this. From php date function
+        $conn->bindParam(':time_read', $time_to_read);
+        $conn->bindParam(':post_image', $post_image);
+        $conn->bindParam(':post_type', $type_of_post);
+        $conn->bindParam(':allow_comments', $allow_comments);
         
         //collect data
         $user = $conn->query("SELECT user_id FROM user WHERE user_id==$user_id");
@@ -68,39 +97,40 @@
 
     function set_blog_post_test(){
         //connect to database
-        $conn = connect();
+        $conn = connect_test();
         
         //prepare statement
-        $sql = "INSERT INTO posts_table (content, date_posted, post_type)
-        VALUES(:content, :'date', :post_type)";
-        $conn->prepare($sql);
-        $conn->bindParam(`:content`, $written_post); //from text box
-        $conn->bindParam(`:date_posted`, $date); //dont worry about this. From php date function
-        $conn->bindParam(`:post_type`, $type_of_post);
+        $sql = "INSERT INTO post_table (content, date_posted, post_type)
+        VALUES(:content, :date_posted, :post_type)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':content', $written_post); //from text box
+        $stmt->bindParam(':date_posted', $date); //dont worry about this. From php date function
+        $stmt->bindParam(':post_type', $type_of_post);
         //collect data
 
         $written_post = $_POST['blog-post'];
-        
+      
         //insert date into date field using php DATE
         $date = date('l jS \of F Y h:i:s A');
 
         $type_of_post = "Blog";
               
-        $conn->exec();
+        $stmt->execute();
         echo "New post created successfully.";
-
+        $last_id = $conn->lastInsertId();
         close_connection();
+        return json_encode($last_id);
     }
 
-    function save_blog_post($user_id){
-        $conn = connect();
-        $sql = "INSERT INTO saved_items (saved_items_id, blogs_id, user_id, content, genre, date_posted, post_type)
-        VALUES(:user_id, :content, :genre, :date_posted, :post_type)";
-    }
+    // function save_blog_post($user_id){
+    //     $conn = connect();
+    //     $sql = "INSERT INTO saved_items (saved_items_id, blogs_id, user_id, content, genre, date_posted, post_type)
+    //     VALUES(:user_id, :content, :genre, :date_posted, :post_type)";
+    // }
 
     function get_title($user_id, $post_id){
         $conn = connect();
-        $sql = "SELECT title FROM posts_table WHERE user_id=$user_id AND post_id=$post_id";
+        $sql = "SELECT title FROM `posts_table` WHERE user_id=$user_id AND post_id=$post_id";
         $title = $conn->query($sql);
         close_connection();
         return json_encode($title);
@@ -108,7 +138,7 @@
 
     function get_genre($user_id, $post_id){
         $conn = connect();
-        $sql = "SELECT genre FROM posts_table WHERE user_id=$user_id AND post_id=$post_id";
+        $sql = "SELECT genre FROM `posts_table` WHERE user_id=$user_id AND post_id=$post_id";
         $genre = $conn->query($sql);
         close_connection();
         return json_encode($genre);
@@ -116,7 +146,7 @@
 
     function get_date_posted($user_id, $post_id){
         $conn = connect();
-        $sql = "SELECT date_posted FROM posts_table WHERE user_id=$user_id AND post_id=$post_id";
+        $sql = "SELECT date_posted FROM `posts_table` WHERE user_id=$user_id AND post_id=$post_id";
         $date_posted = $conn->query($sql);
         close_connection();
         return json_encode($date_posted);
@@ -124,7 +154,7 @@
 
     function get_time_posted($user_id, $post_id){
         $conn = connect();
-        $sql = "SELECT time_posted FROM posts_table WHERE user_id=$user_id AND post_id=$post_id";
+        $sql = "SELECT time_posted FROM `posts_table` WHERE user_id=$user_id AND post_id=$post_id";
         $time_posted = $conn->query($sql);
         close_connection();
         return json_encode($time_posted);
@@ -132,7 +162,7 @@
 
     function get_time_read($user_id, $post_id){
         $conn = connect();
-        $sql = "SELECT time_read FROM posts_table WHERE user_id=$user_id AND post_id=$post_id";
+        $sql = "SELECT time_read FROM `posts_table` WHERE user_id=$user_id AND post_id=$post_id";
         $time_read = $conn->query($sql);
         close_connection();
         return json_encode($time_read);
@@ -140,7 +170,7 @@
 
     function get_post_image($user_id, $post_id){
         $conn = connect();
-        $sql = "SELECT post_image FROM posts_table WHERE user_id=$user_id AND post_id=$post_id";
+        $sql = "SELECT post_image FROM `posts_table` WHERE user_id=$user_id AND post_id=$post_id";
         $post_image = $conn->query($sql);
         close_connection();
         return json_encode($post_image);
@@ -152,7 +182,7 @@
     function get_blog_post($user_id, $post_id){
         //match user name
         $conn = connect();
-        $sql = "SELECT content FROM posts_table WHERE user_id=$user_id AND post_id=$post_id";
+        $sql = "SELECT content FROM `posts_table` WHERE user_id=$user_id AND post_id=$post_id";
         $user_blog = $conn->query($sql);
         close_connection();
         return json_encode($user_blog);
@@ -161,13 +191,19 @@
     function get_blog_post_test(){
         //match user name
         $conn = connect();
-        $sql = "SELECT content FROM posts_table WHERE user_id=$user_id AND post_id=$post_id";
+        $sql = "SELECT content FROM `posts_table` WHERE user_id=$user_id AND post_id=$post_id";
         $user_blog = $conn->query($sql);
         close_connection();
         return json_encode($user_blog);
     }
 
-
+    function allow_comments($post_id){
+        $conn = connect();
+        $sql = "SELECT allow_comments FROM `posts_table` WHERE post_id=$post_id";
+        $allowcomments = $conn->query($sql);
+        close_connection();
+        return json_encode($allowcomments);
+    }
 
     function delete_post($user_id, $post_id){
         $conn = connect();
@@ -180,7 +216,7 @@
         //open connection
         $conn = connect();
         //prepared statement
-        $sql = "INSERT INTO posts_table (user_id, content, genre, date_posted, post_type)
+        $sql = "INSERT INTO `posts_table` (user_id, content, genre, date_posted, post_type)
         VALUES(:user_id, :content, :genre, :date_posted, :post_type)";
         $conn->prepare($sql);
         $conn->bindParam(`:user_id`, $user_id);
@@ -204,7 +240,7 @@
         //open connection
         $conn = connect();
         //prepared statement
-        $sql = "INSERT INTO posts_table (content, date_posted, post_type)
+        $sql = "INSERT INTO `posts_table` (content, date_posted, post_type)
         VALUES(:content, :date_posted, :post_type)";
         $conn->prepare($sql);     
         $conn->bindParam(`:content`, $tip_content);
@@ -223,7 +259,7 @@
 
     function delete_tip($user_id, $tip_id){
         $conn = connect();
-        $postdel = "DELETE FROM `posts_table` WHERE `tips_id` = $tip_id AND `user_id` = $user_id";
+        $postdel = "DELETE FROM ``posts_table`` WHERE `tips_id` = $tip_id AND `user_id` = $user_id";
         $conn->exec($postdel);
         close_connection();
     }
@@ -261,7 +297,10 @@
                 break;    
                 
         }
+        $tag_sql = "SELECT * from tags WHERE post_id = $content_id";
+        $return_tags = $conn->query($tag_sql);
         close_connection();
+        return json_encode($return_tags);
     } */
     
     function get_tag_count($content_id){
@@ -326,7 +365,7 @@
             $user_id = "SELECT user_id FROM feedback WHERE `feedback_id` = $feedback_id";
             $feedback_content = "SELECT content FROM feedback WHERE `feedback_id` = $feedback_id";
             $date = date('l jS \of F Y h:i:s A');
-            $sql = "INSERT INTO posts_table (content, post_type, user_id, date_posted) VALUES ($feedback_content, "Tips", $user_id, $date"); 
+            $sql = "INSERT INTO `posts_table` (content, post_type, user_id, date_posted) VALUES ($feedback_content, "Tips", $user_id, $date"); 
         }
         close_connection();
         
@@ -339,7 +378,7 @@
         //$stmt->bindParam(`:user_id`, $user_id);
         
         $search_result = $conn->prepare($search);
-        $search_result->bindParam(':query', %$query%);
+        $search_result->bindParam(':query', $query);
         $conn->execute();
         $results = $conn->fetchAll();
         return json_encode($results, JSON_FORCE_OBJECT);
@@ -353,7 +392,7 @@
      */
     function post_randomizer(){
         $conn = connect();
-        $sql = "SELECT * FROM post_table WHERE post_type = "Blog"";
+        $sql = "SELECT * FROM post_table WHERE post_type = 'Blog'";
         $conn->query($sql);
         $blogs = $conn->fetchAll(PDO::FETCH_COLUMN, 4); //grabs all content from result
         close_connection();
@@ -361,7 +400,7 @@
         $count = count($blogs);
         $random_num = random_int(0, $count-1);
         $blog_to_return = $blogs[$random_num];
-        return json_encode($blog_to_return, JSON_FORCE_OBJECT);
+        return json_encode($blog_to_return);
     }
 
 
